574e881cce5f5ed8f8026a1a8725924fde1c753e
phathung2001 <phathung2001@gmail.com>
2020-08-12 12:07:26 +0700

    Change argument handler to pointers

--- main.cpp
+++ main.cpp
@@ -1,52 +1,70 @@
 #include "mikilib.h"
 #include "arguments.h"
 
+#define VERSION "2.0"
+
 int main (int argc, char** argv) {
     try {
         vector<argument> args = parseArguments(argc, argv);
 
-        // Yeah I know. Primitive argument handling, very bad performance. Will enhance it later. 
-        // This is just a WIP anyway
+        argument* e_arg = getArg(args, "-e");
+        argument* o_arg = getArg(args, "-o");
 
         // Extract mode
-        if (ifexist(args, "-e")) {
-            if (ifexist(args, "-o")) {
-                extractArchive(getArg(args, "-e").arglst[0], getArg(args, "-o").arglst[0]);
+        if (e_arg) {
+            if (o_arg) {
+                extractArchive(e_arg->arglst[0], o_arg->arglst[0]);
             }
             else {
                 throw runtime_error("No outfile specified.");
             }
+            exit(0);
         }
-        else if (ifexist(args, "-a")) {
+
+        argument* a_arg = getArg(args, "-a");
+        argument* chunksize_arg = getArg(args, "--chunksize");
+
+        if (a_arg) {
             short chunksize = 1024;
-            if (ifexist(args, "--chunksize")) {
-                chunksize = stoi(getArg(args, "--chunksize").arglst[0]);
+            if (chunksize_arg) {
+                chunksize = stoi(chunksize_arg->arglst[0]);
             }
 
-            if (ifexist(args, "-o")) {
-                mkarchive filelst = buildFromFiles(getArg(args, "-a").arglst);
-                writeArchive(filelst, getArg(args, "-o").arglst[0], chunksize);
+            if (o_arg) {
+                mkarchive filelst = buildFromFiles(a_arg->arglst);
+                writeArchive(filelst, o_arg->arglst[0], chunksize);
+
+                cout << "Archive created." << endl;
+                printArchiveContent(filelst);
             }
             else {
                 throw runtime_error("No output directory specified.");
             }
+            exit(0);
         }
-        else if (ifexist(args, "-d")) {
+
+        argument* d_arg = getArg(args, "-d");
+
+        if (d_arg) {
             short chunksize = 1024;
-            if (ifexist(args, "--chunksize")) {
-                chunksize = stoi(getArg(args, "--chunksize").arglst[0]);
+            if (chunksize_arg) {
+                chunksize = stoi(chunksize_arg->arglst[0]);
             }
 
-            if (ifexist(args, "-o")) {
-                mkarchive filelst = buildCurrentDir(getArg(args, "-d").arglst[0]);
-                writeArchive(filelst, getArg(args, "-o").arglst[0], chunksize);
+            if (o_arg) {
+                mkarchive filelst = buildFromDir(d_arg->arglst[0]);
+                writeArchive(filelst, o_arg->arglst[0], chunksize);
+
+                cout << "Archive created." << endl;
+                printArchiveContent(filelst);
             }
             else {
                 throw runtime_error("No output directory specified.");
             }
+            exit(0);
         }
         else {
-            cout << "MikiArchiver v1.0" << endl;
+            cout << "MikiArchiver v" << VERSION << endl;
             cout << "Simple file archiver by hungngocphat01." << endl;
             cout << "Powered by C++14." << endl;
             cout << endl;
@@ -59,6 +77,7 @@ int main (int argc, char** argv) {
     }
     catch (const exception& e) {
         cout << "An error has occurred: " << e.what() << endl;
+        return 1;
     }
     return 0;
 }
