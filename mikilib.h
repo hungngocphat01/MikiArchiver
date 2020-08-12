fc3e6eb666e9fa1533da788fa363abcd74565abc
phathung2001 <phathung2001@gmail.com>
2020-08-12 12:08:17 +0700

    Add printArchiveContent

--- mikilib.h
+++ mikilib.h
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include <string>
+#include <cstring>
 #include <exception>
 #include <stdexcept>
-#include <cstring>
 #include <dirent.h>
 
 using namespace std;
@@ -16,8 +17,9 @@ struct mkfile {
 
 using mkarchive = vector<mkfile>;
 
-mkarchive buildCurrentDir(string path);
+mkarchive buildFromDir(string path);
 mkarchive buildFromFiles(vector<string> filelst);
 
 unsigned writeArchive(mkarchive& archive, string filename, short chunk_size);
 unsigned extractArchive(string filename, string extract_path);
+void printArchiveContent(const mkarchive& archive);
